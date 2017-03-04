6e3ee8f743e07f735695d4b013b2381f7bb3201a
Jerry GJ <jerry_7707@hotmail.com>
2017-03-05 00:40:13 +0800

    Added Container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 101},
+    { title: 'Harry Potter', pages: 21},
+    { title: 'Star Wars', pages: 30},
+    { title: 'The Hobbit', pages: 600}
+  ]
+}
